2b5f5744e87c4ab8d37cb4341ce7fef912bd6a67
Patrick Romão <patrickromao2001@gmail.com>
2025-11-09 13:47:42 -0300

    remove folder

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
+import os
+import github
+from dotenv import load_dotenv
+
 def main():
-    pass
+    load_dotenv()
+    github.download_folder("py-minecraft-sync", "p3tworld", destination='./p3tworld_2')
 
 if __name__ == '__main__':
     main()
